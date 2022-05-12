3f7c54cfa299a6b15095bde226a021e20b587075
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-05-12 20:40:44 +0200

    Added method to extract h2 of main content for important places. Also created result file in content folder

--- crawling-and-preprocessing/WebCrawler.py
+++ crawling-and-preprocessing/WebCrawler.py
@@ -12,19 +12,20 @@ class WebCrawler:
         """
         Crawl website roughguides.com by entries in given csv file
         """
-        guides = pd.read_csv('/content/DataScience2022_RoughGuides.csv');
+        guides = pd.read_csv('content/DataScience2022_RoughGuides.csv');
         guides['introduction'] = None
         guides['description'] = None
+        guides['important_places'] = None
         for row in guides.itertuples():
             url = row[5]
             crawled_page = requests.get(url)
             html_doc = crawled_page.content
             soup = BeautifulSoup(html_doc, 'html.parser')
-            introduction = self.__get_introduction(soup)
-            description = self.__get_description(soup)
-            guides['introduction'].iloc[row.Index] = introduction
-            guides['description'].iloc[row.Index] = description
-        guides.to_csv('/content/crawled_rough_guides.csv', index=False)
+            guides['introduction'].iloc[row.Index] = self.__get_introduction(soup)
+            guides['description'].iloc[row.Index] = self.__get_description(soup)
+            guides['important_places'].iloc[row.Index] = self.__get_important_places(soup)
+            print('crawled ' + str(row.Index) + '/' + str(len(guides)))
+        guides.to_csv('content/crawled_rough_guides.csv', index=False)
 
     @staticmethod
     def __get_introduction(soup):
@@ -49,3 +50,22 @@ class WebCrawler:
             if ('</a>' not in str(child)) and ('class=\"mb-' not in str(child)):
                 description += child.get_text()
         return description
+
+    @staticmethod
+    def __get_important_places(soup):
+        """
+        Get important places of current soup.
+        :return: important places (h2) headlines of main content
+        """
+        important_places = []
+        main = soup.find('div', {'class': ''})
+        headings = main.findChildren('h2')
+        for heading in headings:
+            if (heading.get('id') is not None) and ((not heading.get('id').startswith('discover-more-places') or (
+                    not heading.get('id').startswith('places-to-visit')))):
+                important_places.append(heading.get_text())
+        return important_places
+
+
+if __name__ == '__main__':
+    WebCrawler().crawl_website()
