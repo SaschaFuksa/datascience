4c333121cef65fb224fad3b200b16780a4519b01
SaschaFuksa <SaschaFuksa@users.noreply.github.com>
2022-06-23 13:47:22 +0200

    Now show all four topics as bar charts

--- dash-task2/ComponentBuilder.py
+++ dash-task2/ComponentBuilder.py
@@ -1,9 +1,10 @@
 import nltk
-import plotly.express as px
-from dash import html, dcc
 import plotly.graph_objects as go
+from dash import html, dcc
+
 nltk.download('stopwords')
-import matplotlib.colors as mcolors
+from plotly.subplots import make_subplots
+import dash_bootstrap_components as dbc
 
 
 class ComponentBuilder:
@@ -19,35 +20,52 @@ class ComponentBuilder:
 
     @staticmethod
     def build_topic_charts(topic_file):
-        df1 = topic_file.loc[topic_file.topic_id==0, :]
-
-        fig = go.Figure()
-        fig.add_trace(go.Bar(
-            x=df1['word'],
-            y=df1['word_count'],
-            name='word count',
-            marker_color='indianred'
-        ))
-        fig.add_trace(go.Bar(
-            x=df1['word'],
-            y=df1['word_count'],
-            name='weights',
-            width=0.5,
-            marker_color='lightsalmon'
-        ))
-
-        # Here we modify the tickangle of the xaxis, resulting in rotated labels.
-        fig.update_layout(barmode='group', xaxis_tickangle=-45)
-
-        #fig = px.bar(topic_file, x='word', height='word_count', color='importance')
-        #    ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
-        #ax_twin = ax.twinx()
-        graph = dcc.Graph(id='topic_charts', figure=fig)
+        graph_1 = ComponentBuilder.create_topic_graph(topic_file.loc[topic_file.topic_id == 0, :], 0, 'indianred',
+                                                      'lightsalmon')
+        graph_2 = ComponentBuilder.create_topic_graph(topic_file.loc[topic_file.topic_id == 1, :], 1, 'aqua',
+                                                      'aquamarine')
+        graph_3 = ComponentBuilder.create_topic_graph(topic_file.loc[topic_file.topic_id == 2, :], 2, 'darkkhaki',
+                                                      'darkseagreen')
+        graph_4 = ComponentBuilder.create_topic_graph(topic_file.loc[topic_file.topic_id == 3, :], 3, 'gold',
+                                                      'goldenrod')
 
         return html.Div([
             html.H2('Topic charts'),
-            graph,
+            html.Div(children=[
+                dbc.Row([
+                    dbc.Col(graph_1),
+                    dbc.Col(graph_2)
+                ]),
+                dbc.Row([
+                    dbc.Col(graph_3),
+                    dbc.Col(graph_4)
+                ]),
+            ])
         ])
+
+    @staticmethod
+    def create_topic_graph(df, id, colour_1, colour_2):
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        fig.add_trace(go.Bar(
+            x=df['word'],
+            y=df['word_count'],
+            name='word count',
+            marker_color=colour_1,
+        ), secondary_y=False, )
+        fig.add_trace(go.Bar(
+            x=df['word'],
+            y=df['importance'],
+            name='weights',
+            width=0.3,
+            marker_color=colour_2
+        ), secondary_y=True, )
+
+        fig.update_layout(barmode='group', xaxis_tickangle=-45, margin=dict(l=0, r=0, t=0, b=0),)
+        graph = dcc.Graph(id='topic_charts_' + str(id), figure=fig, config={
+            'displayModeBar': False
+        })
+        return graph
 
     @staticmethod
     def build_top3_places():
